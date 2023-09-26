a217aeeeb059a6fc125e51f493b636bf2c41838d
Gabriel Enríquez <gabrielenlo9@gmail.com>
2023-09-26 12:22:43 +0200

    Añadir Dto Product y Contact

---
+++ src/main/java/com/campusdual/appmazing/model/dto/Contactdto.java
@@ -0,0 +1,69 @@
+package com.campusdual.appmazing.model.dto;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Contactdto {
+
+    private int id;
+
+    private String name;
+
+    private String username;
+
+    private String secondusername;
+
+    private int phone;
+
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSecondusername() {
+        return secondusername;
+    }
+
+    public void setSecondusername(String secondusername) {
+        this.secondusername = secondusername;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

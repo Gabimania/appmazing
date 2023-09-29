486083219d0cb1038a99632f93e8a69adcc58bdf
Gabriel Enríquez <gabrielenlo9@gmail.com>
2023-09-29 11:34:25 +0200

    1. Modified Category. Before that, it was created the controller and the server to Category aswell.

--- src/main/java/com/campusdual/appmazing/model/Category.java
+++ src/main/java/com/campusdual/appmazing/model/Category.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column
     private String name;
